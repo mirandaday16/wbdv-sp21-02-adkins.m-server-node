52f1bd8028923005e75c9e66347c2f28e1fb6677
mirandadayadkins <mirandaday16@gmail.com>
2021-04-04 17:53:09 -0400

    added code for question service and controller

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+
 const quizzesController = require('./controllers/quizzes-controller')
 quizzesController(app)
+
+const questionsController = require('./controllers/question-controller')
+questionsController(app)
 
 app.listen(4000)
