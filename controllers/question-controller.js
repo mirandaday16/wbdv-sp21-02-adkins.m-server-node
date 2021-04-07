7936d30b9a59f585dc214f930411704edaeb89be
mirandadayadkins <mirandaday16@gmail.com>
2021-04-07 15:17:44 -0400

    fixed typo in question-controller

--- controllers/question-controller.js
+++ controllers/question-controller.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
     const findQuestionsForQuiz = (req, res) => {
         const quizId = req.params['qid']
         const questions = questionService.findQuestionsForQuiz(quizId)
-        res.json(questions)
+        res.send(questions)
     }
     app.get("/api/quizzes/:qid/questions",
         findQuestionsForQuiz)
